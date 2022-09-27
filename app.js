d9d9ecd7f58bd44c00568eab13e071aa925fc656
Tom Mingarelli <tmingo0726@gmail.com>
2022-09-27 13:29:51 -0500

    Resolving minor conflicts

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const express = require("express");
 const morgan = require('morgan');
 const router = require('./api/index');
 const client = require('./db/client');
+const cors = require('cors');
 
 const app = express();
+
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
+
 app.use('/api', router);
-//app.use(client);
 
 client.connect();
 
