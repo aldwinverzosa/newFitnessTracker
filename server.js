a86b74164bae1f235af2ea8e6f29da6482cba5e7
aldwinverzosa <aldwinverzosa@gmail.com>
2022-10-01 15:00:44 -0500

    changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const chalk = require("chalk")
 const app = require("./app")
 const express = require('express');
 
-const PORT = process.env["PORT"] ?? 3001
+const { PORT = 3000 } = process.env
 const server = http.createServer(app)
 
 server.listen(PORT, () => {
